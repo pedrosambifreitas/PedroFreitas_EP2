63d668ad3f999eedd33b9e63a347164bb212d1da
pedrosambifreitas <pedrosambifreitas@hotmail.com>
2015-03-26 15:33:22 -0300

    5
    terminando as funcoes de desenho

--- projeto2.py
+++ projeto2.py
@@ -26,6 +26,66 @@ tartaruga.penup()
 tartaruga.left(90)
 
 
+#desenhando a cabeca
+def cabeca():
+	tartaruga.penup()
+	tartaruga.setpos(-150,180)
+	tartaruga.pendown()
+	tartaruga.left(90)
+	tartaruga.circle(15)
+	tartaruga.right(90)
+	
+#desenha do o corpo
+def corpo():
+	tartaruga.penup()
+	tartaruga.setpos(-150,140)
+	tartaruga.forward(100)
+	tartaruga.penup()
+	
+	
+#desenha braco direito
+def bracodireito():
+	tartaruga.penup()
+	tartaruga.setpos(-150,100)
+	tartaruga.right(90)
+	tartaruga.pendow()
+	tartaruga.foward(50)
+	tartaruga.penup()
+	tartaruga.left(90)
+	
+	
+#desenha braco esquerdo	
+def bracoesquerdo():
+	tartaruga.penup()
+	tartaruga.setpos(-150,100)
+	tartaruga.left(90)
+	tartaruga.pendow()
+	tartaruga.foward(50)
+	tartaruga.penup()
+	tartaruga.right(90)
+	
+#desenha perna esquerda
+def pernaesquerda():
+	tartaruga.penup()
+	tartaruga.setpos(-150,40)
+	tartaruga.left(30)
+	tartaruga.pendow()
+	tartaruga.foward(50)
+	tartaruga.penup()
+	tartaruga.right(30)
+	
+#desenha	
+def pernadireita():
+	tartaruga.penup()
+	tartaruga.setpos(-150,40)
+	tartaruga.right(30)
+	tartaruga.pendow()
+	tartaruga.foward(50)
+	tartaruga.penup()
+	tartaruga.left(30)
+
+
+
 #abrindo entrada de texto salvo como entrada.txt, 
 #criando lista vazia e adicionando as palavras limpas na lista
 lista = list()
@@ -37,7 +97,7 @@ for i in conteudo:
 	if palavras != "":
 		lista.append(palavras)
 		
-#computador escolhendo e limpando a palavra
+#computador escolhendo e limpando a palavra, cria lista com as letars separadas
 computador = choice(lista)
 escolha_computador = computador.lower()
 acerto = 0
@@ -46,29 +106,28 @@ tamanho = len(escolha_computador)
 letras = list(escolha_computador)
 
 # tracinhos
+tartaruga.penup()
 tartaruga.speed(20)
-tartaruga.setpos(0,-10)
+tartaruga.setpos(-80,-10)
 for i in range(tamanho):
 	if letras[i] != " ":
 		tartaruga.pendown()
 		tartaruga.color("black")
-		tartaruga.forward(30)
+		tartaruga.forward(25)
 		tartaruga.penup()
 		tartaruga.forward(15)
 	elif letras[i] == " ":
 		tartaruga.penup()
-		tartaruga.forward(15)
+		tartaruga.forward(25)
 		
 	
 		
-	
-"""
+erro = 0
 while erro < 6 and acerto < len(computador): # e os espaços?
 	tentativa = janela.textinput("Escolha uma letra")
 	tentativa = tentativa.lower()
 	# logica do jogo
-	if tentativa in escolha_computador
-"""
+	#if tentativa in escolha_computador:
 
 
 
