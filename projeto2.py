69175c11d1861ad24cd21e1757b38041df29a6a3
pedrosambifreitas <pedrosambifreitas@hotmail.com>
2015-03-26 14:41:56 -0300

    4
    tracinhos comeco

--- projeto2.py
+++ projeto2.py
@@ -9,7 +9,7 @@ janela = turtle.Screen()
 janela.bgcolor("green")
 janela.title("Jogo da Forca")
 
-#desenhando a forca
+#desenhando a forca em funcao 
 tartaruga = turtle.Turtle()
 tartaruga.speed(20)
 tartaruga.penup()
@@ -24,13 +24,44 @@ tartaruga.right(90)
 tartaruga.forward(50)
 
 
-#abrindo entrada de texto salvo como entrada.txt
-entrada = open("entrada.txt","r",encoding ='utf-8')
-palavras = entrada.readlines()
+#abrindo entrada de texto salvo como entrada.txt, 
+#criando lista vazia e adicionando as palavras limpas na lista
+lista = list()
+entrada = open("entrada.txt",encoding ='utf-8')
+conteudo = entrada.readlines()
 
-lista = [0]*11
-lista = [palavras]
+for i in conteudo:
+	palavras=i.strip()
+	if palavras != "":
+		lista.append(palavras)
+		
+#computador escolhendo e limpando a palavra
+computador = choice(lista)
+escolha_computador = computador.lower()
+acerto = 0
+errado = 0
+tamanho = len(escolha_computador)
+
+# tracinhos
+tartaruga.speed(20)
+tartaruga.setpos(-220,-10)
+for i in range(tamanho):
+	if i != "":
+		tartaruga.pendown()
+		tartaruga.color("black")
+		tartaruga.forward(10)
+		tartaruga.penup()
+		tartaruga.forward(5)
+	
+		
+	
+"""
+while erro < 6 and acerto < len(computador): # e os espaços?
+	tentativa = janela.textinput("Escolha uma letra")
+	tentativa = tentativa.lower()
+	# logica do jogo
+	if tentativa in escolha_computador
+"""
 
 
-escolha_computador = choice(lista)
-print(escolha_computador)
+
