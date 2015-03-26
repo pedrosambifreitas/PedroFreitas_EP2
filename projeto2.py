b29d544b01289bc9807f78d09ed4b9e591e86fe5
pedrosambifreitas <pedrosambifreitas@hotmail.com>
2015-03-26 08:00:27 -0300

    commit 1
    abrindo janela, adicionando tartaruga

---
+++ projeto2.py
@@ -0,0 +1,26 @@
+#Pedro Sambi Freitas
+# -*- coding: utf-8 -*-
+
+import random 
+import turtle
+
+#abrindo a janela
+janela = turtle.Screen()
+janela.bgcolor("green")
+janela.title("Jogo da Forca")
+
+#desenhando a forca
+tartaruga = turtle.Turtle()
+tartaruga.speed()
+tartaruga.penup()
+tartaruga.setpos()
+tartaruga.pendown()
+tartaruga.color()
+tartaruga.left()
+tartaruga.foward()
+tartaruga.right()
+tartaruga.foward()
+tartaruga.right()
+tartaruga.foward()
+
+
